d8161f20df7c7f9371db82861a7fc1b04bcf433b
Manish kumar <134037752+Manish-kumar-9601@users.noreply.github.com>
2024-05-22 20:54:30 +0530

    connection with DB
    Signed-off-by: Manish kumar <134037752+Manish-kumar-9601@users.noreply.github.com>

---
+++ src/db/index.js
@@ -0,0 +1,21 @@
+﻿import mongoose from "mongoose";
+import dotenv from 'dotenv';
+import { DB_NAME } from "../constants.js";
+    dotenv.config({
+        path: './env'
+    })
+const connectDB = async () =>
+{
+    try
+    {
+        mongoose.connect(`${process.env.DB_URL}/${DB_NAME}`)
+        console.log("successful connnected with DB")
+    } catch (error)
+    {
+        console.error("connection failed with DB", error)
+        process.exit(1)
+    }
+
+}
+
+export default connectDB;
